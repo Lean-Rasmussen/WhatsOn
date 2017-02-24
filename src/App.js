5e0bb12f1c73460d7835812bbad3296af11dd528
Lean Rasmussen <leanmkrasmussen@gmail.com>
2017-02-24 16:39:58 +0100

    very basic layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,22 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Whats On</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <from>
+        <label> select a provider</label>
+          <select>
+            <option>api1</option>
+          </select>
+          <label>Search</label>
+          <input type='search'></input> 
+          <button>Search</button>
+
+        </from>
+        <div className='TVcontainer'>
+          <div className='playerContainer'><h1>Player</h1></div>
+          <div className='channelSelector'><h2>My favorit channels</h2></div>   
+         </div>      
       </div>
     );
   }
