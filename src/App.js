b06592a4b100c2af1b4ff0456e4dc524ff2f5b17
Lean Rasmussen <leanmkrasmussen@gmail.com>
2017-03-05 18:03:24 +0100

    added channels for test

--- src/App.js
+++ src/App.js
@@ -10,9 +10,12 @@ import './App.css';
     this.state={
       src: 'https://www.youtube.com/embed/?el=adunit&controls=0&html5=1&playsinline=1&ps=gvn&showinfo=0&widget_referrer=http%3A%2F%2Fplayer.twitch.tv%2F%3Fchannel%3Dmedrybw&enablejsapi=1&origin=http%3A%2F%2Fimasdk.googleapis.com&widgetid=1',
       video:'https://drive.google.com/uc?export=download&id=0B0JMGMGgxp9WMEdWb1hyQUhlOWs',
-      testSrc:'http://player.twitch.tv/?channel=medrybw&muted=true',
-      favorits:[],
-      twitchBaseLink:'http://player.twitch.tv/?channel=',
+      activeStream:'http://player.twitch.tv/?channel=medrybw&muted=true',
+      favorits:[{name:"Jeffs channel",
+                id:112,
+                img:'http://www.allcscollectibles.com/wp-content/uploads/2014/03/MTGlogo.jpg',
+                link: 'http://player.twitch.tv/?channel=tjcollectibles',
+    }],
     }
   }
 
@@ -28,15 +31,23 @@ import './App.css';
         this.state.favorits.push(newFavorit);
       document.getElementById("streamNameInput").value =''
 }
+//Saving and loading from Local storage
+  saveToLocalStorage(){
+    window.localStorage.setItem('favorits', JSON.stringify(this.state.favorits));
+  }
 
-saveToLocalStorage(){
-console.log('saving')
-
-}
 loadFromLocalStorage(){
-  console.log('loading')
+  this.setState({
+    favorits: JSON.parse(window.localStorage.getItem("favorits")),
+  })
+}
+//Changing channel
+changeChannel(){
 
+  this.setState({
+    activeStream:'http://player.twitch.tv/?channel=tjcollectibles',
 
+  })
 }
 
   render() {
@@ -63,7 +74,7 @@ loadFromLocalStorage(){
           <div className='playerContainer'>
             <h1>Player</h1>
             <div className='player'>
-            <iframe src={this.state.testSrdc} autoPlay='false' height="400" width="600" frameBorder="0" scrolling="no"
+            <iframe src={this.state.activeStream} autoPlay='false' height="400" width="600" frameBorder="0" scrolling="no"
     allowFullScreen="true">
 </iframe>
             </div>
@@ -71,9 +82,11 @@ loadFromLocalStorage(){
           <div className='channelSelector'>
             <h2>My favorit channels</h2>
             <button>Load Api</button>
-            {this.state.favorits.map((channel,id) =>{
+            {this.state.favorits.map((channel) =>{
               return(
-                <div className='favorits' id={id}>{channel.name}</div>
+                <div onClick= {()=> this.changeChannel()} className='favorits' id={channel.id}>
+                  <img className='streamImg' src= {channel.img} ></img>
+                {channel.name}</div>
               )}
               )
               }
